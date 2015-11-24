11f83ff2f0a0abc49ef3307c9aaff341862fca66
Peter Halliday <pghalliday@gmail.com>
2015-11-25 00:06:32 +0100

    uchiwa basic config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 default['formatron_uchiwa']['version'] = '0.13.0.1'
 default['formatron_uchiwa']['checksum'] = '84adc0e9c00a0a131ae88f8b707c28c2c95b6688dd53e8cd573e2d91bc9e52c2'
+default['formatron_uchiwa']['host'] = '0.0.0.0'
+default['formatron_uchiwa']['port'] = 3000
+default['formatron_uchiwa']['sensu']['name'] = node['fqdn']
+default['formatron_uchiwa']['sensu']['host'] = node['fqdn']
+default['formatron_uchiwa']['sensu']['port'] = 4567
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,10 @@
 version = node['formatron_uchiwa']['version']
 checksum = node['formatron_uchiwa']['checksum']
+host = node['formatron_uchiwa']['host']
+port = node['formatron_uchiwa']['port']
+sensu_name = node['formatron_uchiwa']['sensu']['name']
+sensu_host = node['formatron_uchiwa']['sensu']['host']
+sensu_port = node['formatron_uchiwa']['sensu']['port']
 
 cache = Chef::Config[:file_cache_path]
 deb = File.join cache, 'uchiwa.deb' 
@@ -14,6 +19,17 @@ end
 dpkg_package 'uchiwa' do
   source deb
   action :nothing
+  notifies :restart, 'service[uchiwa]', :delayed
+end
+
+template '/etc/sensu/uchiwa.json' do
+  variables(
+    host: host,
+    port: port,
+    sensu_name: sensu_name,
+    sensu_host: sensu_host,
+    sensu_port: sensu_port
+  )
   notifies :restart, 'service[uchiwa]', :delayed
 end
 
