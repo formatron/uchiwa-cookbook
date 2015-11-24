ca9052f190956ff59a2768b7429669d0826caea4
Peter Halliday <pghalliday@gmail.com>
2015-11-25 00:50:09 +0100

    correct version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['formatron_uchiwa']['version'] = '0.13.0.1'
+default['formatron_uchiwa']['version'] = '0.13.0-1'
 default['formatron_uchiwa']['checksum'] = '84adc0e9c00a0a131ae88f8b707c28c2c95b6688dd53e8cd573e2d91bc9e52c2'
 default['formatron_uchiwa']['host'] = '0.0.0.0'
 default['formatron_uchiwa']['port'] = 3000
